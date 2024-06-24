eff16d3e853ad126460259e5c4676182d2dfc3ed
aseplamri <aseplamri12@gmail.com>
2024-06-24 14:59:13 +0700

    t

--- Test Suites/Test_Login.ts
+++ Test Suites/Test_Login.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>328579f7-1e5b-4fc5-9191-90256fc74623</testSuiteGuid>
+   <testCaseLink>
+      <guid>d63ef299-1620-4467-a107-887d158d97ae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
