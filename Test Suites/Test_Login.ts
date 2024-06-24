de1ca9338762ab07799bff7f21e8c59a34799c9d
aseplamri <aseplamri12@gmail.com>
2024-06-24 14:42:12 +0700

    qqq

---
+++ Test Suites/Test_Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>328579f7-1e5b-4fc5-9191-90256fc74623</testSuiteGuid>
+</TestSuiteEntity>
